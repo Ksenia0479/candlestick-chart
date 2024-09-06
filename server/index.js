5652c1c5cb14decc4100f02fe5fa203373e9df3b
Ksenia <kseniyachervonaya@gmail.com>
2024-09-06 15:32:28 +0200

    refactor: Update server console log message to include the port number

--- server/index.js
+++ server/index.js
@@ -14,11 +14,11 @@ app.use(
 app.get("/url", async (req, res) => {
   const from = req.query.from;
   const to = req.query.to;
-  let urlRUB = `https://charts.forexpf.ru/html/tw/history?symbol=29&resolution=5&from=${from}&to=${to}`;
+  const urlRUB = `https://charts.forexpf.ru/html/tw/history?symbol=29&resolution=5&from=${from}&to=${to}`;
   const { data } = await axios.get(urlRUB);
   res.json(data);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT} port`);
 });
